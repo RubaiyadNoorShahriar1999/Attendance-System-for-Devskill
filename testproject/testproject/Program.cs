1e22074786c98ebfff34be6f066cdca7cb825877
Rubaiyad Noor Shahriar <rubaiyadnoorshahriar@gmail.com>
2022-12-24 05:35:53 +0600

    Restructured the Project Infrastructure

--- testproject/testproject/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using testProject;
-
-testProjectDbContext test = new testProjectDbContext();
-Student s1 = new Student();
-s1.Name= "Test";
-s1.Description= "This is a test";
-
-test.Add(s1);
-test.SaveChanges();
